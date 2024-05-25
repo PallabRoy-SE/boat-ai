129bc84e68b8eb99176a2c2390dfb95fbfaeb658
PALLAB ROY <76547199+PallabRoy-SE@users.noreply.github.com>
2024-05-25 16:25:57 +0530

    fix: font fix for no history text ( refs #5 )

--- src/pages/history/History.jsx
+++ src/pages/history/History.jsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import BtiHistoryCard from "../../components/histroy-card/BtiHistoryCard";
-import { colors } from "../../theme/variable";
+import { colors, fonts } from "../../theme/variable";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { getDateStringFromNow } from "../../services/helperService";
 function History() {
@@ -33,6 +33,7 @@ function History() {
             variant="h3"
             textAlign="center"
             color={colors.textGrey}
+            fontFamily={fonts.openSans}
             sx={{ opacity: 0.5, fontSize: { xs: "1.5rem", sm: "3rem" } }}
           >
             No history Available.
